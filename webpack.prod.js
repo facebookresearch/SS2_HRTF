965db7c33bedafebffc41cc7546163ffa7641c4f
Saveliy <saveliy@fb.com>
2022-09-26 18:08:07 -0700

    try fix automation

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,14 @@
 const { merge } = require('webpack-merge')
+const path = require('path')
 const common = require('./webpack.common.js')
 
+const OutputDir = path.resolve(__dirname, 'docs')
+
+
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    output: {
+        filename: '[name].js',
+        path: OutputDir
+    },
 })
