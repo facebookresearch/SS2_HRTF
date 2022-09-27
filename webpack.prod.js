572d143f41c73dfcc7f1c2e5eac6a72eff7f08e3
Saveliy <saveliy@fb.com>
2022-09-27 08:40:51 -0700

    switch to gh-pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,13 +2,6 @@ const { merge } = require('webpack-merge')
 const path = require('path')
 const common = require('./webpack.common.js')
 
-const OutputDir = path.resolve(__dirname, 'docs')
-
-
 module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        filename: '[name].js',
-        path: OutputDir
-    },
+    mode: 'production'
 })
